9df4e3918701b36fb9f942796028dd0f5118d26e
koshieguchi <koshieguegu@gmail.com>
2024-09-06 21:26:00 +0900

    Use static library instead of dynamic library

--- pcm/build_pcm.sh
+++ pcm/build_pcm.sh
@@ -1,20 +1,20 @@
 #!/bin/bash -eux
 
 # fetch third-party dependencies
-(
-  # Install prometheus-cpp
-  if [ ! -d "prometheus-cpp" ]; then
-    git clone --recursive https://github.com/jupp0r/prometheus-cpp.git --depth 1
-  fi
+# (
+#   # Install prometheus-cpp
+#   if [ ! -d "prometheus-cpp" ]; then
+#     git clone --recursive https://github.com/jupp0r/prometheus-cpp.git --depth 1
+#   fi
 
-  mkdir -p prometheus-cpp/_build
-  cd prometheus-cpp/_build
+#   mkdir -p prometheus-cpp/_build
+#   cd prometheus-cpp/_build
 
-  cmake .. -DBUILD_SHARED_LIBS=ON -DENABLE_PUSH=OFF -DENABLE_COMPRESSION=OFF -DCMAKE_BUILD_TYPE=Release # run cmake
-  cmake --build . --parallel $(($(nproc)/2)) # build
-  ctest -V # run tests
-  cmake --install . # install the libraries and headers
-) &
+#   cmake .. -DBUILD_SHARED_LIBS=OFF -DENABLE_PUSH=OFF -DENABLE_COMPRESSION=OFF -DCMAKE_BUILD_TYPE=Release # run cmake
+#   cmake --build . --parallel $(($(nproc)/2)) # build
+#   ctest -V # run tests
+#   cmake --install . # install the libraries and headers
+# ) &
 (
   # Install pcm
   if [ ! -d "pcm" ]; then
@@ -25,7 +25,7 @@
   cd pcm/build
 
   cmake ..
-  cmake --build .  --target PCM_SHARED --parallel $(($(nproc)/2)) # build
+  cmake --build . --target PCM_STATIC --parallel $(($(nproc)/2)) # build
 ) &
 
 # wait for both to finish
@@ -44,8 +44,7 @@ mkdir -p ./bin
 g++ -fsanitize=address -g -o ./bin/iio-exporter.out iio-exporter.cpp \
   -I. \
   -I./pcm/src \
-  -L./pcm/build/lib \
-  -lpcm \
+  ./pcm/build/src/libpcm.a \
   -lprometheus-cpp-pull \
   -lprometheus-cpp-core \
   -lz
